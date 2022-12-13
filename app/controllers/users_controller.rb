a670a6a87277ef9cc8a36aa895311bd357348d3e
cynthia <cchelangat9892gmail.com>
2022-12-13 11:52:33 +0300

    Completed Assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
     def show
         user = User.find_by(id: session[:user_id])
         if user
-            render json: user
+          render json: user
         else
-            render json: {error: "User not found"}, status: 404
+          render json: { error: "Not authorized" }, status: :unauthorized
         end
-    end
+      end
 end
