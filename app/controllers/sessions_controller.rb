dbee5503238eae9d039973a1f6a9bd5e20d32704
cynthia <cchelangat9892gmail.com>
2022-12-13 11:52:11 +0300

    commit tree 8d84f78a2d62c0502490e08c17171f41909a28f9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,13 @@
 class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:username])
-        if user 
-            session[:user_id] = user.id
-            render json: user
-        else
-            render json: {error: "User not found"}, status: 404
-        end
-    end
-
-    def destroy
-        session.delete "user_id"
+        session[:user_id] = user.id
+        render json: user
+      end
+      # delete '/logout' => 'sessions#destroy'
+      def destroy
+        session.delete(:user_id)
         head :no_content
-    end
+      end
 end
  
